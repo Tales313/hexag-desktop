22722faab7b633d153c05be4add22f2e34aca512
Tales313 <tales.11.98@gmail.com>
2021-12-22 21:44:48 -0300

    # 0.0.2-SNAPSHOT # > Criando pacotes e adicionando configurações de modulo

---
+++ src/main/java/module-info.java
@@ -0,0 +1,23 @@
+module conta.desktop {
+
+    // usar conta sistema
+    requires conta.sistema;
+
+    // usar spring
+    requires javax.inject;
+    requires spring.tx;
+    requires spring.core;
+    requires spring.beans;
+    requires spring.context;
+    requires java.sql;
+
+    // usar javafx
+    requires javafx.controls;
+
+    // abre telas e builds
+    opens conta.tela;
+    opens conta.dev;
+    opens conta.hml;
+    opens conta.prd;
+
+}
